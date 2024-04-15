6b1be4808986af21536d1ea14e0a7877fd13590c
yemyoungwoo <tkddlektks@gmail.com>
2024-04-16 01:19:10 +0900

    orderList erorr 수정

--- src/main/java/ymw/delivery/controller/GlobalExceptionHandler.java
+++ src/main/java/ymw/delivery/controller/GlobalExceptionHandler.java
@@ -9,6 +9,5 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public String handleException() {
         return"order/orderList"; // 에러 페이지의 뷰 이름
-        
     }
 }
